72ab2e9815f411a44c02566087d7c007ca1a7f95
PranshuTripathi <98465599+wollvex@users.noreply.github.com>
2024-10-09 14:57:11 +0530

    Update communityRoutes.js

--- servers - Copy/routes/communityRoutes.js
+++ servers - Copy/routes/communityRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const communityController = require('../controllers/communityController');
 
 // Routes for communities
-router.get('/communities ', communityController.getAllCommunities);
-router.post('/communities', communityController.createCommunity);
+router.get('/community ', communityController.getAllCommunities);
+router.post('/community', communityController.createCommunity);
 
 module.exports = router;
